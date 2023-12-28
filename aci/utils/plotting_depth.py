dc1ee360b521a758459fd5ef7f23fca1a7455401
Erin Young <erin.olde@gmail.com>
2023-12-28 17:26:38 +0000

    transposed dataframe for graphing

--- aci/utils/plotting_depth.py
+++ aci/utils/plotting_depth.py
@@ -1,28 +1,35 @@
 #!/usr/bin/env python
 
-""" Graphs the dataframe """
+""" Getting coverage ready for plotting """
 
 import logging
-import matplotlib.pyplot as plt
 
-from .split_dataframe import split_dataframe
-from .group_create import group_create
-from .group_mean import group_mean
+from .split_dataframe   import split_dataframe
+from .plotting_boxplot  import plotting_boxplot
 
 def plotting_depth(df, out):
-    """ graphs the dataframe """
+    """ Getting coverage ready for plotting """
 
     df = split_dataframe(df)
 
     # writing results to a file
     df.to_csv(out + '/overall_depth.csv', index=False)
 
+    # restructuring for boxplot
+    df['ungroup'] = df['group'] * 500
     df = df.drop('pos', axis=1)
+    df = df.drop('group', axis=1)
+    df = df.set_index('ungroup')
+    df = df.transpose()
 
-    boxplot = df.boxplot(fontsize=5, rot=90, figsize=(15,8), grid=False)
-    boxplot.plot()
-    plt.title('Overall coverage')
-    boxplot.set_ylabel('meandepth')
-    boxplot.set_xlabel('region')
-    boxplot.figure.savefig(out + '/overall_depth.png', bbox_inches='tight')
-    plt.close()
+    logging.debug('Restructured coverage boxplot')
+    logging.debug(df)
+
+    d = {
+        'title' : 'Overall coverage',
+        'ylabel' : 'meandepth',
+        'xlabel' : 'position',
+        'file' : out + '/overall_depth.png'
+    }
+
+    plotting_boxplot(df,d)
