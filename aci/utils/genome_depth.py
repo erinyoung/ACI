372e9a42e185b8691e210d07ab17350a81c3ba53
Erin Young <erin.olde@gmail.com>
2024-01-16 17:35:59 -0700

    adjusting logging

--- aci/utils/genome_depth.py
+++ aci/utils/genome_depth.py
@@ -10,7 +10,7 @@ def genome_depth(meta):
     """ Takes a bam file file and gets coverage """
 
     # getting depth results from pysam
-    df = pd.DataFrame([x.split('\t') for x in pysam.depth(meta['sorted_bam']).split('\n')])
+    df = pd.DataFrame([x.split('\t') for x in pysam.depth(meta['sorted_bam']).split('\n')]) # pylint: disable=E1101
     df.columns = ['ref', 'pos', 'cov']
 
     # ensuring that the type is correct
@@ -19,7 +19,7 @@ def genome_depth(meta):
     df['bam'] = meta['file_name']
     df = df.dropna()
 
-    logging.debug('pysam results for ' + meta['sorted_bam']) # pylint: disable=W1201
+    logging.debug(f"pysam results for {meta['sorted_bam']}") # pylint: disable=W1203
     logging.debug(df)
 
     return df
