95a6193f2637348ed6d4862aa32c9de7925fb48b
erinyoung <eriny@utah.gov>
2025-07-11 16:39:13 -0600

    removed meta dict

--- aci/utils/genome_depth.py
+++ aci/utils/genome_depth.py
@@ -4,25 +4,26 @@
 
 import logging
 import pandas as pd
+import os
 import pysam
 
 
-def genome_depth(meta):
+def genome_depth(bam):
     """Takes a bam file file and gets coverage"""
 
     # getting depth results from pysam
     df = pd.DataFrame(
-        [x.split("\t") for x in pysam.depth(meta["sorted_bam"]).split("\n")]
+        [x.split("\t") for x in pysam.depth(bam).split("\n")]
     )  # pylint: disable=E1101
     df.columns = ["ref", "pos", "cov"]
 
     # ensuring that the type is correct
     df["pos"] = pd.to_numeric(df["pos"], errors="coerce")
     df["cov"] = pd.to_numeric(df["cov"], errors="coerce")
-    df["bam"] = meta["file_name"]
+    df["bam"] = os.path.basename(bam)
     df = df.dropna()
 
-    logging.debug(f"pysam results for {meta['sorted_bam']}")  # pylint: disable=W1203
+    logging.debug(f"pysam results for {os.path.basename(bam)}")  # pylint: disable=W1203
     logging.debug(df)
 
     return df
